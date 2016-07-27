6a01eb22cc0f1e8ac13c64f08024ef64ea94b037
elichyb <elichyb@gmail.com>
2016-07-27 23:07:54 +0300

    need to do rethinking on the parse enum typedef

--- header.h
+++ header.h
@@ -9,6 +9,11 @@ typedef int bool;
 #define true 1
 #define false 0
 
+/*  enum that list the posiible commands  */
+typedef enum {mov = 0, cmp = 1, add = 2, sub = 3, not = 4, clr = 5
+				lea = 6, inc = 7, dec = 8, jmp = 9, bne = 10, red = 11, 
+				prn = 12, jsr = 13, rts = 14, stop = 15} command;
+
 /*  maximum size of symbol is 30  */
 #define MAX_SYMBOL 31
 
@@ -17,10 +22,12 @@ typedef int bool;
 
 /*  define dirction maximum size*/
 #define DIR_SIZE 6
+
 /*  
 	symbol table:
 	Symbol table struct presents a line in the table
 */
+
 typedef struct symbol_table
 {
 	char symbol_name[MAX_SYMBOL];
